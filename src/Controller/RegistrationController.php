c788b5ca9ea6983fd587a4d14110385708831ecf
Azer Ben Azzouz <benazzouzazer@gmail.com>
2024-05-05 18:41:03 +0100

    chore: Update email sender address in RegistrationController

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -44,7 +44,8 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('no-reply@demomailtrap.com', 'SymBook'))
+                    // ->from(new Address('no-reply@demomailtrap.com', 'SymBook'))
+                    ->from(new Address('benazzouzazer@gmail.com', 'SymBook'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
