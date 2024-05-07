de0fd8b0622086409d0f4690720b62922289f302
Azer Ben Azzouz <benazzouzazer@gmail.com>
2024-05-07 21:17:57 +0100

    Add Reset Password

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -78,6 +78,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_login');
+        return $this->redirectToRoute('app_home');
     }
 }
