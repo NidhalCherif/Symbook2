7a5c0b37a26aa860d59c72d3eb180c636973ea0d
Azer Ben Azzouz <benazzouzazer@gmail.com>
2024-05-12 20:26:08 +0100

    chore: Update doctrine/doctrine-fixtures-bundle to version 3.6

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoriesRepository;
 use App\Repository\LivresRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,13 +11,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(LivresRepository $rep): Response
+    public function index(LivresRepository $rep,CategoriesRepository $rep1): Response
     {
         // show the last 8 new books
         $livres = $rep->findBy([], ['editedAt' => 'DESC'], 8);
+        $categories = $rep1->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'livres' => $livres
+            'livres' => $livres,
+            'categories' => $categories
         ]);
     }
     // tous les livres
